93a28a9f4aaad36fff734a8755f9ea174f19c5b7
rilay <77292122+Rui-1004@users.noreply.github.com>
2025-08-25 22:19:48 +0100

    Add styling in all pages Add 500 error page Add error handlers Add homepage content Add navbar

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const productsRouter = require('./routes/productsRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
 const indexRouter = require('./routes/indexRouter');
 
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Views
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+// Routers
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
+
+// Error handler
+app.all(/.*/, (req, res, next) => {
+  res.status(404).render('404');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('500', { error: err });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,12 @@
-exports.getIndex = (req, res) => {
-  res.render('index', { title: 'Homepage' });
+const { getAllProducts, getAllCategories } = require('../db/queries');
+
+exports.getIndex = async (req, res) => {
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
+
+  res.render('index', {
+    title: 'Dashboard',
+    productCount: products.length,
+    categoryCount: categories.length,
+  });
 };
